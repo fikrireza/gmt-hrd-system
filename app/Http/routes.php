1ef40b87ac4b3bfed996e5b8f5d10b2cc8aa9b2f
dudyali <dudyali@gmail.com>
2016-04-16 15:05:40 +0700

    tambah dashboard

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::get('dashboard', function(){
+  return view('pages/dashboard');
 });
---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,56 @@
+<section class="sidebar">
+  <!-- Sidebar user panel -->
+  <div class="user-panel">
+    <div class="pull-left image">
+      <img src="{{ asset('/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+    </div>
+    <div class="pull-left info">
+      <p>Alexander Pierce</p>
+      <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+    </div>
+  </div>
+  <!-- search form -->
+  <form action="#" method="get" class="sidebar-form">
+    <div class="input-group">
+      <input type="text" name="q" class="form-control" placeholder="Search...">
+      <span class="input-group-btn">
+        <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
+      </span>
+    </div>
+  </form>
+  <!-- /.search form -->
+  <!-- sidebar menu: : style can be found in sidebar.less -->
+  <ul class="sidebar-menu">
+    <li class="header">MAIN NAVIGATION</li>
+    <li class="treeview">
+      <a href="#">
+        <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
+      </a>
+      <ul class="treeview-menu">
+        <li><a href="{{ url('/admin') }}"><i class="fa fa-circle-o"></i> Dashboard</a></li>
+      </ul>
+    </li>
+    <li class="treeview">
+      <a href="#">
+        <i class="fa fa-pie-chart"></i>
+        <span>Kelola Produk</span>
+        <i class="fa fa-angle-left pull-right"></i>
+      </a>
+      <ul class="treeview-menu">
+        <li><a href="{{ url('/product') }}"><i class="fa fa-circle-o"></i> Lihat Semua Produk</a></li>
+        <li><a href="{{ url('/product/create') }}"><i class="fa fa-circle-o"></i> Tambah Produk Baru</a></li>
+      </ul>
+    </li>
+    <li class="treeview">
+      <a href="#">
+        <i class="fa fa-laptop"></i>
+        <span>Kelola Kategori Produk</span>
+        <i class="fa fa-angle-left pull-right"></i>
+      </a>
+      <ul class="treeview-menu">
+        <li><a href="{{ url('/kategori') }}"><i class="fa fa-circle-o"></i> Lihat Semua Kategori Produk</a></li>
+        <li><a href="{{ url('/kategori/create') }}"><i class="fa fa-circle-o"></i> Tambah Kategori Baru</a></li>
+      </ul>
+    </li>
+  </ul>
+</section>
