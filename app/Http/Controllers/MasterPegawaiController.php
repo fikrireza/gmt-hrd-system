3af69ae47dc7e04f60a272388182f87b12d4a480
Fikri Reza Alhadi <fikrirezaa@gmail.com>
2016-05-21 22:47:01 +0700

    tambah data pegawai done

--- app/Http/Controllers/MasterPegawaiController.php
+++ app/Http/Controllers/MasterPegawaiController.php
@@ -10,8 +10,12 @@ use App\Http\Requests;
 use App\Http\Requests\MasterPegawaiRequest;
 use App\MasterPegawai;
 use App\Models\DataKeluarga;
-use App\Models\PengalamanKerja;
 use App\Models\KondisiKesehatan;
+use App\Models\PengalamanKerja;
+use App\Models\Pendidikan;
+use App\Models\BahasaAsing;
+use App\Models\KeahlianKomputer;
+use App\Models\RiwayatPenyakit;
 
 use App\MasterJabatan;
 use Datatables;
@@ -50,6 +54,7 @@ class MasterPegawaiController extends Controller
      */
     public function store(MasterPegawaiRequest $request)
     {
+      //dd($request);
       DB::transaction(function() use($request) {
         $pegawai = MasterPegawai::create([
                       'nip'           => $request->nip,
@@ -82,34 +87,83 @@ class MasterPegawaiController extends Controller
                       'id_pegawai'    => $pegawai->id
         ]);
 
-        /*
-         * Belum masukin foreign key id_pegawai
-         * $pegawai->id inject into array
-         *
-         */
-        $keluarga = $request->input('data_keluarga');
-        dd($keluarga);
-        DB::table('data_keluarga')->insert($keluarga);
+        $keluargas = $request->input('data_keluarga');
+        if($keluargas == !null){
+          foreach($keluargas as $keluarga){
+            $isiKeluarga = new DataKeluarga;
+            $isiKeluarga->nama_keluarga           = $keluarga['nama_keluarga'];
+            $isiKeluarga->hubungan_keluarga       = $keluarga['hubungan_keluarga'];
+            $isiKeluarga->tanggal_lahir_keluarga  = $keluarga['tanggal_lahir_keluarga'];
+            $isiKeluarga->alamat_keluarga         = $keluarga['alamat_keluarga'];
+            $isiKeluarga->jenis_kelamin_keluarga  = $keluarga['jenis_kelamin_keluarga'];
+            $isiKeluarga->pekerjaan_keluarga      = $keluarga['pekerjaan_keluarga'];
+            $isiKeluarga->id_pegawai              = $pegawai->id;
+            $isiKeluarga->save();
+          }
+        }
 
-        $pengalaman_kerja = $request->input('pengalaman');
-        dd($pengalaman_kerja);
-        DB::table('pengalaman_kerja')->insert($pengalaman_kerja);
+        $pengalaman_kerjas = $request->input('pengalaman');
+        if($pengalaman_kerjas == !null){
+          foreach($pengalaman_kerjas as $pengalaman_kerja){
+            $isiPengalaman  = new PengalamanKerja;
+            $isiPengalaman->nama_perusahaan   = $pengalaman_kerja['nama_perusahaan'];
+            $isiPengalaman->posisi_perusahaan = $pengalaman_kerja['posisi'];
+            $isiPengalaman->tahun_awal_kerja  = $pengalaman_kerja['tahun_awal_kerja'];
+            $isiPengalaman->tahun_akhir_kerja = $pengalaman_kerja['tahun_akhir_kerja'];
+            $isiPengalaman->id_pegawai        = $pegawai->id;
+            $isiPengalaman->save();
+          }
+        }
 
-        $pendidikan = $request->input('pendidikan');
-        dd($pendidikan);
-        DB::table('pendidikan')->insert($pendidikan);
+        $pendidikans = $request->input('pendidikan');
+        if($pendidikans == !null){
+          foreach($pendidikans as $pendidikan){
+            $isiPendidikan  = new Pendidikan;
+            $isiPendidikan->jenjang_pendidikan      = $pendidikan['jenjang_pendidikan'];
+            $isiPendidikan->institusi_pendidikan    = $pendidikan['institusi_pendidikan'];
+            $isiPendidikan->tahun_masuk_pendidikan  = $pendidikan['tahun_masuk_pendidikan'];
+            $isiPendidikan->tahun_lulus_pendidikan  = $pendidikan['tahun_lulus_pendidikan'];
+            $isiPendidikan->gelar_akademik          = $pendidikan['gelar_akademik'];
+            $isiPendidikan->id_pegawai              = $pegawai->id;
+            $isiPendidikan->save();
+          }
+        }
 
-        $bahasa = $request->input('bahasa');
-        dd($bahasa);
-        DB::table('bahasa_asing')->insert($bahasa);
+        $bahasas = $request->input('bahasa');
+        if($bahasas == !null){
+          foreach ($bahasas as $bahasa) {
+            $isiBahasa = new BahasaAsing;
+            $isiBahasa->bahasa      = $bahasa['bahasa'];
+            $isiBahasa->berbicara   = $bahasa['berbicara'];
+            $isiBahasa->menulis     = $bahasa['menulis'];
+            $isiBahasa->mengerti    = $bahasa['mengerti'];
+            $isiBahasa->id_pegawai  = $pegawai->id;
+            $isiBahasa->save();
+          }
+        }
 
-        $komputer = $request->insert('komputer');
-        dd($komputer);
-        DB::table('keahlian_komputer')->insert($komputer);
+        $komputers = $request->input('komputer');
+        if($komputers == !null){
+          foreach ($komputers as $komputer) {
+            $isiKomputer  = new KeahlianKomputer;
+            $isiKomputer->nama_program  = $komputer['nama_program'];
+            $isiKomputer->nilai_komputer= $komputer['nilai'];
+            $isiKomputer->id_pegawai    = $pegawai->id;
+            $isiKomputer->save();
+          }
+        }
 
-        $penyakit = $request->input('penyakit');
-        dd($penyakit);
-        DB::table('riwayat_penyakit')->insert($penyakit);
+        $penyakits = $request->input('penyakit');
+        if($penyakits == !null){
+          foreach ($penyakits as $penyakit) {
+            $isiPenyakit  = new RiwayatPenyakit;
+            $isiPenyakit->nama_penyakit = $penyakit['nama_penyakit'];
+            $isiPenyakit->keterangan_penyakit    = $penyakit['keterangan'];
+            $isiPenyakit->id_pegawai    = $pegawai->id;
+            $isiPenyakit->save();
+          }
+        }
+
       });
 
       return redirect()->route('masterpegawai.create')->with('message','Berhasil memasukkan pegawai baru.');
---
+++ app/Models/BahasaAsing.php
@@ -0,0 +1,27 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BahasaAsing extends Model  {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'bahasa_asing';
+
+  protected $fillable = [
+    'bahasa', 'berbicara', 'menulis', 'mengerti', 'id_pegawai'
+  ];
+	/**
+	 * Many to Many relation
+	 *
+	 * @return Illuminate\Database\Eloquent\Relations\belongToMany
+	 */
+	public function master_pegawai()
+	{
+		return $this->belongsToMany('App\MasterPegawai');
+	}
+
+}
