b4bc19560a3e1ed9f674cac797fc30db545c3667
fikrireza <fikrirezaa@gmail.com>
2016-10-31 21:00:17 +0700

    Pagination Position

--- applications/resources/views/pages/MasterClient/index.blade.php
+++ applications/resources/views/pages/MasterClient/index.blade.php
@@ -61,52 +61,25 @@
             <li><a href="{{ url('masterclient/cabang', $client->id)}}">Cabang Client <span class="pull-right badge bg-blue">{{ $client->hitungCabang}}</span></a></li>
           </ul>
         </div>
-      </div><!-- /.widget-user -->
+      </div>
     </div>
     @endforeach
-     @if(!$CountAll->isEmpty())
-       <div style="text-align: center; vertical-align: middle;">
-         {{ $CountAll->links() }}
-       </div>
-     @endif
-  </div><!-- /.row -->
+    </div>
+    @if(!$CountAll->isEmpty())
+    <div class="row">
+      <div style="text-align: center; vertical-align: middle;">
+        {{ $CountAll->links() }}
+      </div>
+    </div>
+    @endif
 
 
   <script src="{{ asset('/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
-  <!-- jQuery UI 1.11.4 -->
-  <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-  <script>
-    $.widget.bridge('uibutton', $.ui.button);
-  </script>
-  <!-- Bootstrap 3.3.5 -->
   <script src="{{ asset('/bootstrap/js/bootstrap.min.js') }}"></script>
-  <!-- Morris.js charts -->
-  <script src="{{ asset('/bootstrap/js/raphael-min.js') }}"></script>
-  <script src="{{ asset('/plugins/morris/morris.min.js') }}"></script>
-  <!-- Sparkline -->
-  <script src="{{ asset('/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
-  <!-- jvectormap -->
-  <script src="{{ asset('/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-  <script src="{{ asset('/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
-  <!-- jQuery Knob Chart -->
-  <script src="{{ asset('/plugins/knob/jquery.knob.js') }}"></script>
-  <!-- daterangepicker -->
-  <script src="{{ asset('/bootstrap/js/moment.min.js') }}"></script>
-  <script src="{{ asset('/plugins/daterangepicker/daterangepicker.js') }}"></script>
-  <!-- datepicker -->
-  <script src="{{ asset('/plugins/datepicker/bootstrap-datepicker.js') }}"></script>
-  <!-- Bootstrap WYSIHTML5 -->
-  <script src="{{ asset('/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
-  <!-- Slimscroll -->
   <script src="{{ asset('/plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
-  <!-- FastClick -->
   <script src="{{ asset('plugins/fastclick/fastclick.min.js') }}"></script>
-  <!-- AdminLTE App -->
   <script src="{{ asset('dist/js/app.min.js') }}"></script>
-  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
   <script src="{{ asset('/dist/js/pages/dashboard.js') }}"></script>
-  <!-- AdminLTE for demo purposes -->
   <script src="{{ asset('/dist/js/demo.js') }}"></script>
 
 
