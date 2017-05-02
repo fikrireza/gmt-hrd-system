33ae1c1badb32a420c574334bd8e8c9b68f2af7e
Bayu widia <bayuwidiasantoso@gmail.com>
2017-05-02 17:00:24 +0700

    progress history gaji pokok

--- applications/resources/views/pages/detailbatchpayroll.blade.php
+++ applications/resources/views/pages/detailbatchpayroll.blade.php
@@ -99,7 +99,7 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Set Komponen Gaji</h4>
+            <h4 class="modal-title">Set Absensi</h4>
           </div>
           <div class="modal-body">
             <div class="form-group">
