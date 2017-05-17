27cf38121a5bd68af284eecf4e13ff055abe0169
fikrireza <fikrirezaa@gmail.com>
2017-05-17 17:33:17 +0700

    Export report payroll per client

--- applications/app/Http/Controllers/LaporanBatchPayrollController.php
+++ applications/app/Http/Controllers/LaporanBatchPayrollController.php
@@ -17,6 +17,7 @@ use App\Models\KomponenGajiTetap;
 use App\Models\DetailPeriodeGaji;
 use App\Models\DetailKomponenGaji;
 use App\Models\DetailBatchPayroll;
+use App\Models\CabangClient;
 
 use DB;
 use Excel;
@@ -87,21 +88,16 @@ class LaporanBatchPayrollController extends Controller
 
     public function prosesAll($id)
     {
-      // Get Data Supervisi Pegawwai
-      $getSPV = PKWT::join('detail_batch_payroll', 'detail_batch_payroll.id_pegawai', '=', 'data_pkwt.id_pegawai')
-                      ->join('master_pegawai', 'master_pegawai.id', '=', 'data_pkwt.id_kelompok_jabatan')
-                      ->select('master_pegawai.nama','detail_batch_payroll.id_pegawai', 'data_pkwt.id_kelompok_jabatan')
-                      ->where('detail_batch_payroll.id_batch_payroll', $id)
-                      ->orderby('data_pkwt.id_kelompok_jabatan')
-                      ->groupby('master_pegawai.id')
-                      ->get();
 
-      $getAnak = PKWT::join('detail_batch_payroll', 'detail_batch_payroll.id_pegawai', '=', 'data_pkwt.id_pegawai')
-                      ->join('master_pegawai', 'master_pegawai.id', '=', 'data_pkwt.id_kelompok_jabatan')
-                      ->select('detail_batch_payroll.id_pegawai', 'data_pkwt.id_kelompok_jabatan')
-                      ->where('detail_batch_payroll.id_batch_payroll', $id)
-                      ->orderby('data_pkwt.id_kelompok_jabatan')
-                      ->get();
+      $getCabangClient = DB::select("SELECT a.id, a.nama_cabang
+                                      FROM cabang_client a, data_pkwt b, detail_batch_payroll c, master_pegawai d
+                                      WHERE a.id = b.id_cabang_client
+                                      AND c.id_pegawai = d.id
+                                      AND b.id_pegawai = c.id_pegawai
+                                      AND c.id_batch_payroll = $id
+                                      GROUP BY a.nama_cabang");
+      $getCabangClient = collect($getCabangClient);
+      // dd($getCabangClient);
 
       $getkomponengajinya = KomponenGaji::orderby('tipe_komponen', 'asc')->get();
 
@@ -110,10 +106,11 @@ class LaporanBatchPayrollController extends Controller
                                 ->where('batch_payroll.id', $id)->first();
 
       // Start Query Detail Gaji Karyawan
-      $query1 = "SELECT pegawai.id, pegawai.nip, pegawai.nama as nama_pegawai, IFNULL(tabel_Workday.workday, 0) as Jumlah_Workday, IFNULL(tabel_Jabatan.nama_jabatan, 0) as Jabatan";
+      $query1 = "SELECT pegawai.id, pegawai.nip, pegawai.nama as nama_pegawai, IFNULL(tabel_Workday.workday, 0) as Jumlah_Workday, IFNULL(tabel_Jabatan.nama_jabatan, 0) as Jabatan, IFNULL(tabel_Cabang.id_cabang, 0) as Cabang ";
       $query2 = "FROM (select a.id, a.nip, a.nama from master_pegawai a, detail_batch_payroll where a.id = detail_batch_payroll.id_pegawai and detail_batch_payroll.id_batch_payroll = 41) as pegawai ";
       $query3 = "LEFT OUTER JOIN (SELECT d.id, d.nama, c.workday as workday FROM komponen_gaji a, detail_komponen_gaji b, detail_batch_payroll c, master_pegawai d WHERE b.id_komponen_gaji = a.id AND b.id_detail_batch_payroll = c.id AND d.id = c.id_pegawai AND c.id_batch_payroll = $id GROUP BY d.id) as tabel_Workday ON pegawai.id = tabel_Workday.id ";
-      $query4 = "LEFT OUTER JOIN(SELECT b.nama_jabatan, a.id FROM master_pegawai a, master_jabatan b WHERE a.id_jabatan = b.id) as tabel_Jabatan ON pegawai.id = tabel_Jabatan.id";
+      $query4 = "LEFT OUTER JOIN(SELECT b.nama_jabatan, a.id FROM master_pegawai a, master_jabatan b WHERE a.id_jabatan = b.id) as tabel_Jabatan ON pegawai.id = tabel_Jabatan.id ";
+      $query5 = "LEFT OUTER JOIN(SELECT a.id as id_cabang, c.id_pegawai as id_pegawai FROM cabang_client a, data_pkwt b, detail_batch_payroll c WHERE a.id = b.id_cabang_client AND b.id_pegawai = c.id_pegawai AND c.id_batch_payroll = $id) as tabel_Cabang ON pegawai.id = tabel_Cabang.id_pegawai ";
       foreach ($getkomponengajinya as $komponen) {
         $replace = str_replace(' ', '_', $komponen->nama_komponen);
         $query1 .=  ",IFNULL(tabel_$replace.nilai, 0) as Jumlah_$replace ";
@@ -123,19 +120,21 @@ class LaporanBatchPayrollController extends Controller
 
       $getkomponengaji = KomponenGaji::orderby('tipe_komponen', 'asc')->get();
 
-      $hasilQuery = DB::select($query1.$query2.$query3.$query4);
+      $hasilQuery = DB::select($query1.$query2.$query3.$query4.$query5);
       $hasilQuery = collect($hasilQuery);
+// dd($hasilQuery);
 
 
-      Excel::create('Proses Payrol SPV Periode -'.$getbatch->tanggal_proses.' s-d '.$getbatch->tanggal_proses_akhir, function($excel) use($getkomponengaji,$getbatch,$hasilQuery) {
-          $excel->sheet('All Payroll', function($sheet) use($getkomponengaji,$getbatch,$hasilQuery) {
+
+      Excel::create('Proses Payrol SPV Periode -'.$getbatch->tanggal_proses.' s-d '.$getbatch->tanggal_proses_akhir, function($excel) use($getkomponengaji,$getbatch,$hasilQuery,$getCabangClient) {
+          $excel->sheet('All Payroll', function($sheet) use($getkomponengaji,$getbatch,$hasilQuery,$getCabangClient) {
             $sheet->loadView('pages.LaporanPayroll.allProses')
                     ->with('getkomponengaji', $getkomponengaji)
                     ->with('getbatch', $getbatch)
+                    ->with('getCabangClient', $getCabangClient)
                     ->with('hasilQuery', $hasilQuery);
           });
       })->download('xlsx');
-
 
       return redirect()->back();
     }
--- applications/resources/views/pages/LaporanPayroll/allProses.blade.php
+++ applications/resources/views/pages/LaporanPayroll/allProses.blade.php
@@ -25,12 +25,38 @@
         @endforeach
         <td style="background-color: #cccdce;"><b>TOTAL GAJI</b></td>
       </tr>
-    @php
+    @foreach ($getCabangClient as $client)
+      @php
       $no = 1;
       $grandJumlahGaji = 0;
       $grandTotalGaji = 0;
-    @endphp
+      $JumlahGaji = 0;
+      $TotalGaji = 0;
+      $Jumlah_Workday = 0;
+      $Jumlah_GAJI_POKOK = 0;
+      $Jumlah_TUNJANGAN_JABATAN = 0;
+      $Jumlah_TUNJANGAN_INSENTIF = 0;
+      $Jumlah_TUNJANGAN_LEMBUR = 0;
+      $Jumlah_KEKURANGAN_BULAN_LALU = 0;
+      $Jumlah_TUNJANGAN_TRANSPORT_MAKAN = 0;
+      $Jumlah_KETUA_REGU = 0;
+      $Jumlah_PENGEMBALIAN_SERAGAM = 0;
+      $Jumlah_TUNJANGAN_MAKAN_LEMBUR = 0;
+      $Jumlah_SALARY = 0;
+      $Jumlah_SHIFT_PAGI_SIANG = 0;
+      $Jumlah_TUNJANGAN_MAKAN_TRANSPORT = 0;
+      $Jumlah_POTONGAN_KAS = 0;
+      $Jumlah_BPJS_KESEHATAN = 0;
+      $Jumlah_BPJS_KETENAGAKERJAAN = 0;
+      $Jumlah_POTONGAN_PINJAMAN = 0;
+      $Jumlah_POTONGAN_SERAGAM = 0;
+      $Jumlah_POTONGAN_CONSUMABLE = 0;
+      @endphp
+      <tr style="border: 1px solid black;">
+        <td style="background-color: #cccdce;" colspan="25">{{ $client->nama_cabang}}</td>
+      </tr>
     @foreach($hasilQuery as $key)
+    @if($key->Cabang == $client->id)
       <tr style="border: 1px solid black;">
         <td style="border: 1px solid black;">{{ $no }}</td>
         <td style="border: 1px solid black;">{{$key->nip }}</td>
@@ -50,6 +76,20 @@
         <td style="border: 1px solid black;">{{$key->Jumlah_SHIFT_PAGI_SIANG }}</td>
         <td style="border: 1px solid black;">{{$key->Jumlah_TUNJANGAN_MAKAN_TRANSPORT }}</td>
         @php
+          $Jumlah_Workday += $key->Jumlah_Workday;
+          $Jumlah_GAJI_POKOK += $key->Jumlah_GAJI_POKOK;
+          $Jumlah_TUNJANGAN_JABATAN += $key->Jumlah_TUNJANGAN_JABATAN;
+          $Jumlah_TUNJANGAN_INSENTIF += $key->Jumlah_TUNJANGAN_INSENTIF;
+          $Jumlah_TUNJANGAN_LEMBUR += $key->Jumlah_TUNJANGAN_LEMBUR;
+          $Jumlah_KEKURANGAN_BULAN_LALU += $key->Jumlah_KEKURANGAN_BULAN_LALU;
+          $Jumlah_TUNJANGAN_TRANSPORT_MAKAN += $key->Jumlah_TUNJANGAN_TRANSPORT_MAKAN;
+          $Jumlah_KETUA_REGU += $key->Jumlah_KETUA_REGU;
+          $Jumlah_PENGEMBALIAN_SERAGAM += $key->Jumlah_PENGEMBALIAN_SERAGAM;
+          $Jumlah_TUNJANGAN_MAKAN_LEMBUR += $key->Jumlah_TUNJANGAN_MAKAN_LEMBUR;
+          $Jumlah_SALARY += $key->Jumlah_SALARY;
+          $Jumlah_SHIFT_PAGI_SIANG += $key->Jumlah_SHIFT_PAGI_SIANG;
+          $Jumlah_TUNJANGAN_MAKAN_TRANSPORT += $key->Jumlah_TUNJANGAN_MAKAN_TRANSPORT;
+
           $jumlahGajinya = $key->Jumlah_GAJI_POKOK + $key->Jumlah_TUNJANGAN_JABATAN + $key->Jumlah_TUNJANGAN_INSENTIF + $key->Jumlah_TUNJANGAN_LEMBUR + $key->Jumlah_KEKURANGAN_BULAN_LALU + $key->Jumlah_TUNJANGAN_TRANSPORT_MAKAN + $key->Jumlah_KETUA_REGU + $key->Jumlah_PENGEMBALIAN_SERAGAM + $key->Jumlah_TUNJANGAN_MAKAN_LEMBUR + $key->Jumlah_SALARY + $key->Jumlah_SHIFT_PAGI_SIANG + $key->Jumlah_TUNJANGAN_MAKAN_TRANSPORT;
 
           $grandJumlahGaji += $jumlahGajinya;
@@ -62,39 +102,81 @@
         <td style="border: 1px solid black;">{{$key->Jumlah_POTONGAN_SERAGAM }}</td>
         <td style="border: 1px solid black;">{{$key->Jumlah_POTONGAN_CONSUMABLE }}</td>
         @php
+          $Jumlah_BPJS_KESEHATAN += $key->Jumlah_BPJS_KESEHATAN;
+          $Jumlah_POTONGAN_KAS += $key->Jumlah_POTONGAN_KAS;
+          $Jumlah_BPJS_KETENAGAKERJAAN += $key->Jumlah_BPJS_KETENAGAKERJAAN;
+          $Jumlah_POTONGAN_PINJAMAN += $key->Jumlah_POTONGAN_PINJAMAN;
+          $Jumlah_POTONGAN_SERAGAM += $key->Jumlah_POTONGAN_SERAGAM;
+          $Jumlah_POTONGAN_CONSUMABLE += $key->Jumlah_POTONGAN_CONSUMABLE;
+
           $jumlahPotongannya = $key->Jumlah_BPJS_KESEHATAN + $key->Jumlah_POTONGAN_KAS + $key->Jumlah_BPJS_KETENAGAKERJAAN + $key->Jumlah_POTONGAN_PINJAMAN + $key->Jumlah_POTONGAN_SERAGAM + $key->Jumlah_POTONGAN_CONSUMABLE;
           $no++;
           $grandTotalGaji += $jumlahGajinya - $jumlahPotongannya;
         @endphp
         <td style="background-color: #cccdce; border: 1px solid black;">{{ $jumlahGajinya - $jumlahPotongannya}}</td>
       </tr>
+    @endif
     @endforeach
+    @php
+      $JumlahGaji += $grandJumlahGaji;
+      $TotalGaji += $grandTotalGaji;
+    @endphp
       <tr style="background-color: #cccdce;">
         <td colspan="2" style="border: 1px solid black;"><b>Sub Total</b></td>
         <td style="border: 1px solid black;"></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_Workday')}}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_Workday }}</b></td>
         <td style="border: 1px solid black;"></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_GAJI_POKOK')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_TUNJANGAN_JABATAN')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_TUNJANGAN_INSENTIF')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_TUNJANGAN_LEMBUR')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_KEKURANGAN_BULAN_LALU')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_TUNJANGAN_TRANSPORT_MAKAN')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_KETUA_REGU')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_PENGEMBALIAN_SERAGAM')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_TUNJANGAN_MAKAN_LEMBUR')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_SALARY')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_SHIFT_PAGI_SIANG')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_TUNJANGAN_MAKAN_TRANSPORT')}}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_GAJI_POKOK }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_TUNJANGAN_JABATAN }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_TUNJANGAN_INSENTIF }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_TUNJANGAN_LEMBUR }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_KEKURANGAN_BULAN_LALU }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_TUNJANGAN_TRANSPORT_MAKAN }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_KETUA_REGU }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_PENGEMBALIAN_SERAGAM }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_TUNJANGAN_MAKAN_LEMBUR }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_SALARY }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_SHIFT_PAGI_SIANG }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_TUNJANGAN_MAKAN_TRANSPORT }}</b></td>
         <td style="border: 1px solid black;"><b>{{ $grandJumlahGaji }}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_BPJS_KESEHATAN')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_POTONGAN_KAS')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_BPJS_KETENAGAKERJAAN')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_POTONGAN_PINJAMAN')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_POTONGAN_SERAGAM')}}</b></td>
-        <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_POTONGAN_CONSUMABLE')}}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_BPJS_KESEHATAN }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_POTONGAN_KAS }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_BPJS_KETENAGAKERJAAN }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_POTONGAN_PINJAMAN }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_POTONGAN_SERAGAM }}</b></td>
+        <td style="border: 1px solid black;"><b>{{ $Jumlah_POTONGAN_CONSUMABLE }}</b></td>
         <td style="border: 1px solid black;"><b>{{ $grandTotalGaji }}</td>
       </tr>
+    @endforeach
+    <tr>
+      <td colspan="25">&nbsp;</td>
+    </tr>
+    <tr style="background-color: #cccdce;">
+      <td colspan="2" style="border: 1px solid black;"><b>Sub Total</b></td>
+      <td style="border: 1px solid black;"></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_Workday') }}</b></td>
+      <td style="border: 1px solid black;"></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_GAJI_POKOK') }}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_TUNJANGAN_JABATAN') }}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_TUNJANGAN_INSENTIF') }}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_TUNJANGAN_LEMBUR') }}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_KEKURANGAN_BULAN_LALU') }}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_TUNJANGAN_TRANSPORT_MAKAN') }}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_KETUA_REGU')}}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_PENGEMBALIAN_SERAGAM')}}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_TUNJANGAN_MAKAN_LEMBUR')}}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_SALARY')}}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_SHIFT_PAGI_SIANG')}}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_TUNJANGAN_MAKAN_TRANSPORT')}}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $JumlahGaji }}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_BPJS_KESEHATAN')}}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_POTONGAN_KAS')}}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_BPJS_KETENAGAKERJAAN')}}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_POTONGAN_PINJAMAN')}}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_POTONGAN_SERAGAM')}}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $hasilQuery->sum('Jumlah_POTONGAN_CONSUMABLE')}}</b></td>
+      <td style="border: 1px solid black;"><b>{{ $TotalGaji }}</td>
+    </tr>
     </table>
 
 </html>
